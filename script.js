6401ebaf9b069f87827ba3ddfbd84b690ce0b1ef
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-11-14 17:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 window.promises = [];
-
+let op = document.getElementById("output");
 // Do not change the code above this
 // add your promises to the array `promises`
+let p1 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve("P1 Resolved")
+	},1000)
+});
+let p2 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve("P2 Resolved")
+	},2000)
+});
+let p3 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve("P3 Resolved")
+	},3000)
+});
+let p4 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve("P4 Resolved")
+	},4000)
+});
+let p5 = new Promise((resolve)=>{
+	setTimeout(()=>{
+		resolve("P5 Resolved")
+	},5000)
+});
+
+Promise.any([p1,p2,p3,p4,p5]).then(res=>{
+	return op.textContent=res;
+})
