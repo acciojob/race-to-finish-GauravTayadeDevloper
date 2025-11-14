a436be6314de992789b153c5aec2f2bbffbda537
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-11-14 19:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,32 +2,18 @@ window.promises = [];
 let op = document.getElementById("output");
 // Do not change the code above this
 // add your promises to the array `promises`
-let p1 = new Promise((resolve)=>{
+for(let i=0;i<5;i++)
+	{
+		let timer = Math.Floor(Math.random()*5)+1;
+		let p1 = new Promise((resolve)=>{
 	setTimeout(()=>{
 		resolve("P1 Resolved")
-	},1000)
-});
-let p2 = new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve("P2 Resolved")
-	},2000)
-});
-let p3 = new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve("P3 Resolved")
-	},3000)
-});
-let p4 = new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve("P4 Resolved")
-	},4000)
-});
-let p5 = new Promise((resolve)=>{
-	setTimeout(()=>{
-		resolve("P5 Resolved")
-	},5000)
-});
+	},timer * 1000)
+	});	
+		window.promises.push(p1);
+	}
 
-Promise.any([p1,p2,p3,p4,p5]).then(res=>{
+
+Promise.any(window.promises).then(res=>{
 	return op.textContent=res;
 })
