d451fbf909c24222338574a3ff612a5756be35bf
GAURAV  TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2025-11-14 19:02:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.promises = [];
 let op = document.getElementById("output");
 // Do not change the code above this
 // add your promises to the array `promises`
-for(let i=0;i<5;i++)
+for(let i=1;i<=5;i++)
 	{
-		let timer = Math.Floor(Math.random()*5)+1;
+		let timer = Math.floor(Math.random()*5)+1;
 		let p1 = new Promise((resolve)=>{
 	setTimeout(()=>{
-		resolve("P1 Resolved")
+		resolve(`P${i} Resolved`)
 	},timer * 1000)
 	});	
 		window.promises.push(p1);
